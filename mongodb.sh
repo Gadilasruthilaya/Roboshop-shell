81473a942821d2e32b5a48a18415d61a9433d7d2
sruthilaya <sruthilayagadila@gmail.com>
2023-05-29 18:00:09 +0530

    Mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,13 +1,13 @@
 
 echo -e "\e[31m Copying mongo repo to files of repos\e[0m"
-cp mongodb.repo etc/yum.repos.d/mongo.repo
+cp mongodb.repo etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
 echo -e "\e[31mInstalling mongodb\e[0m"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>/tmp/roboshop.log
 
 
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
 
 echo -e "\e[31m Enabling and restarting nginx\e[0m"
-systemctl enable mongod
-systemctl restart mongod
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log
