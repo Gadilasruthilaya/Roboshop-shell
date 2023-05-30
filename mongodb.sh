5a566c038df941afddd87628ab5e249b184754a5
sruthilaya <sruthilayagadila@gmail.com>
2023-05-30 15:38:18 +0530

    change listen address in Mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -6,7 +6,9 @@ echo -e "\e[31mInstalling mongodb\e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
 
 
-#Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
+echo -e "\e[31mUpdate listen address\e[0m"
+
+sed -e 's/127.0.0.1/0.0.0.0' /etc/mongo.conf
 
 echo -e "\e[31m Enabling and restarting nginx\e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
