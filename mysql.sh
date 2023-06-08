7ece7bc3edfe3971742ddd5a0ff30eb21e74cc76
sruthilaya <sruthilayagadila@gmail.com>
2023-06-08 17:18:25 +0530

    Mysql component and service

---
+++ mysql.sh
@@ -0,0 +1,13 @@
+
+echo -e "\e[31m Disable my sql module\e[0m"
+yum module disable mysql -y &>>/tmp/roboshop.log
+
+echo -e "\e[31m  Install mysql community server\e[0m"
+yum install mysql-community-server -y &>>/tmp/roboshop.log
+
+echo -e "\e[31m start mysql server\e[0m"
+systemctl enable mysqld &>>/tmp/roboshop.log
+systemctl start mysqld &>>/tmp/roboshop.log
+
+echo -e "\e[31m setup Mysql password\e[0m"
+mysql_secure_installation --set-root-pass RoboShop@1 &>>/tmp/roboshop.log
