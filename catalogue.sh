db3fcc00c177ff35b22ee904bc03639e7f3cab42
sruthilaya <sruthilayagadila@gmail.com>
2023-06-11 16:40:37 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,6 +1,6 @@
 
 source common.sh
-component= catalogue
+component=catalogue
 
 
 nodejs
--- common.sh
+++ common.sh
@@ -1,5 +1,5 @@
-color=  "\e[31m"
-no_color= "\e[0m"
+color= "\e[31m"
+no_color="\e[0m"
 file_path="/tmp/roboshop.log"
 app_path="/app"
 
