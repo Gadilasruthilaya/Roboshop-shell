70468f10512faaf4844fa820cb5df64bedaee2f4
sruthilaya <sruthilayagadila@gmail.com>
2023-06-11 16:36:20 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,40 +1,15 @@
-echo -e "\e[31m configuring nodejs\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
 
-echo -e "\e[31m Installing nodejs\e[0m"
-yum install nodejs -y &>>/tmp/roboshop.log
+source common.sh
+component= catalogue
 
-echo -e "\e[31m Add application user\e[0m"
-useradd roboshop &>>/tmp/roboshop.log
 
-echo -e "\e[31m Creat application directory\e[0m"
-rm -rf /app &>>/tmp/roboshop.log
-mkdir /app
+nodejs
 
-echo -e "\e[31m Download application content \e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
-cd /app
+echo -e " ${color} copying MongoDB repo file ${no_color}"
+cp /home/centos/Roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${file_path}
 
-echo -e "\e[31m  Extract application Conetent\e[0m"
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
-cd /app &>>/tmp/roboshop.log
+echo -e " ${color} Install MongoDb client  ${no_color}"
+yum install mongodb-org-shell -y &>>${file_path}
 
-echo -e "\e[31m Install nodejs dependencies\e[0m"
-npm install &>>/tmp/roboshop.log
-
-echo -e "\e[31m setup Systemd service \e[0m"
-cp /home/centos/Roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
-
-echo -e "\e[31m start catalogue service\e[0m"
-systemctl daemon-reload  &>>/tmp/roboshop.log
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl start catalogue &>>/tmp/roboshop.log
-
-echo -e "\e[31m copying MongoDB repo file\e[0m"
-cp /home/centos/Roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
-
-echo -e "\e[31m Install MongoDb client \e[0m"
-yum install mongodb-org-shell -y &>>/tmp/roboshop.log
-
-echo -e "\e[31m Load schema\e[0m"
-mongo --host mongodb-dev.devopspractice.store </app/schema/catalogue.js &>>/tmp/roboshop.log
+echo -e " ${color}' Load schema ${no_color}"
+mongo --host mongodb-dev.devopspractice.store <$app_path/schema/${component}.js &>>${file_path}'
---
+++ common.sh
@@ -0,0 +1,39 @@
+color=  "\e[31m"
+no_color= "\e[0m"
+file_path="/tmp/roboshop.log"
+app_path="/app"
+
+nodejs(){
+
+  echo -e " ${color} configuring nodejs ${no_color}"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${file_path}
+
+  echo -e " ${color} Installing nodejs ${no_color}"
+  yum install nodejs -y &>>${file_path}
+
+  echo -e " ${color} Add application user ${no_color}"
+  useradd roboshop &>>${file_path}
+
+  echo -e " ${color} Creat application directory ${no_color}"
+  rm -rf $app_path &>>${file_path}
+  mkdir $app_path
+
+  echo -e " ${color} Download application content  ${no_color}"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${file_path}
+  cd $app_path
+
+  echo -e "${color}  Extract application Conetent ${no_color}"
+  unzip /tmp/${component}.zip &>>${file_path}
+  cd $app_path &>>${file_path}
+
+  echo -e " ${color} Install nodejs dependencies ${no_color}"
+  npm install &>>${file_path}
+
+  echo -e " ${color} setup Systemd service  ${no_color}"
+  cp /home/centos/Roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>${file_path}
+
+  echo -e " ${color} start ${component}service ${no_color}"
+  systemctl daemon-reload  &>>${file_path}
+  systemctl enable${component}&>>${file_path}
+  systemctl start ${component} &>>${file_path}
+}
